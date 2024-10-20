99ae7298e3c4d75f419d763ae3d9c7a6f280f90c
Mattseph <105849780+Mattseph@users.noreply.github.com>
2024-10-20 16:37:50 +0800

    Edit: Adminstrator -> Administrator

--- app/Http/Middleware/AdminMiddleware.php
+++ app/Http/Middleware/AdminMiddleware.php
@@ -16,7 +16,7 @@ class AdminMiddleware
      */
     public function handle(Request $request, Closure $next): Response
     {
-        if (Auth::check() && Auth::user()->role === 'Adminstrator') {
+        if (Auth::check() && Auth::user()->role === 'Administrator') {
 
             return $next($request);
         }
