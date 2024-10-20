7f6a27552c2da4986df21f41c2c8bb3af842ce28
Mattseph <105849780+Mattseph@users.noreply.github.com>
2024-10-20 16:36:20 +0800

    Add: SelectInput.vue Components

---
+++ resources/js/Components/SelectInput.vue
@@ -0,0 +1,18 @@
+<script setup>
+const model = defineModel({
+    type: String,
+    required: true,
+});
+
+</script>
+
+<template>
+
+<select class="rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500" v-model="model">
+    <option value="" disabled hidden selected>Select</option>
+    <option value="Administrator">Administrator</option>
+    <option value="Customer">Customer</option>
+</select>
+
+
+</template>
