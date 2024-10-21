6d811f8561e7d92f3f8b027c17e66c72193b5413
Mattseph <105849780+Mattseph@users.noreply.github.com>
2024-10-21 15:01:21 +0800

    Add: AdminLayout.vue Layout

---
+++ resources/js/Pages/Admin/Layout/AdminLayout.vue
@@ -0,0 +1,18 @@
+<script setup>
+import Header from "@/Pages/Admin/Components/Header.vue";
+import Sidebar from "@/Pages/Admin/Components/Sidebar.vue";
+</script>
+
+<template>
+    <div class="antialiased bg-gray-50 dark:bg-gray-900">
+        <!-- Header -->
+
+        <Header />
+
+        <!-- Sidebar -->
+
+        <Sidebar />
+
+        <slot />
+    </div>
+</template>
