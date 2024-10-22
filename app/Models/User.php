82a9e79904c43bb91c27292aae8b22c5f25abea9
Mattseph <105849780+Mattseph@users.noreply.github.com>
2024-10-22 14:45:26 +0800

    Edit: Add products and orders Table Relationship

--- app/Models/User.php
+++ app/Models/User.php
@@ -46,4 +46,13 @@ class User extends Authenticatable
             'password' => 'hashed',
         ];
     }
+
+
+    public function user_addresses () {
+        $this->hasMany(UserAddress::class);
+    }
+
+    public function order() {
+        $this->hasMany(Order::class);
+    }
 }
