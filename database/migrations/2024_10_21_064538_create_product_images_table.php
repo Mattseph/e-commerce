1ac8eaee379c8c3985ab1fd3bfba6e662a46041a
Mattseph <105849780+Mattseph@users.noreply.github.com>
2024-10-22 14:52:03 +0800

    Edit: Add ProductImages Table Fields

--- database/migrations/2024_10_21_064538_create_product_images_table.php
+++ database/migrations/2024_10_21_064538_create_product_images_table.php
@@ -13,6 +13,8 @@ return new class extends Migration
     {
         Schema::create('product_images', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('product_id')->constrained('products')->cascadeOnUpdate()->cascadeOnDelete();
+            $table->string('image');
             $table->timestamps();
         });
     }
