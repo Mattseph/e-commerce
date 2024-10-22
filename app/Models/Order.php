e5ba0fe55259223d6327c6cdf6ad62835ecde460
Mattseph <105849780+Mattseph@users.noreply.github.com>
2024-10-22 14:41:41 +0800

    Edit: Add users, user_addresses, payments, and order_items Table Relationship

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -6,5 +6,21 @@ use Illuminate\Database\Eloquent\Model;
 
 class Order extends Model
 {
-    //
+    public function user() {
+        $this->belongsTo(User::class);
+    }
+
+    public function user_address() {
+        $this->belongsTo(UserAddress::class);
+    }
+
+    public function order_items() {
+        $this->hasMany(OrderItem::class);
+    }
+
+    public function payment () {
+        $this->hasOne(Payment::class);
+    }
+
+
 }
