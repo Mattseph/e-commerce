674f46115c016f5add52c551ba8d51c0b6d69ffe
Mattseph <105849780+Mattseph@users.noreply.github.com>
2024-10-22 14:44:27 +0800

    Edit: Add categories, brands, cart_items, and product_images Table Relationship

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -6,5 +6,19 @@ use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
-    //
+    public function category() {
+        $this->belongsTo(Category::class);
+    }
+
+    public function brand() {
+        $this->belongsTo(Brand::class);
+    }
+
+    public function cart_items() {
+        $this->hasMany(CartItem::class);
+    }
+
+    public function product_images() {
+        $this->hasMany(ProductImage::class);
+    }
 }
