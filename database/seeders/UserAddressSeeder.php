a75c26989a481848a04a4fb8e3b6bc558d26b6e3
Mattseph <105849780+Mattseph@users.noreply.github.com>
2024-10-23 11:43:45 +0800

    Edit: Add UserAddress Factory

--- database/seeders/UserAddressSeeder.php
+++ database/seeders/UserAddressSeeder.php
@@ -2,8 +2,9 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\UserAddress;
 use Illuminate\Database\Seeder;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
 class UserAddressSeeder extends Seeder
 {
@@ -12,6 +13,6 @@ class UserAddressSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        UserAddress::factory(5)->create();
     }
 }
