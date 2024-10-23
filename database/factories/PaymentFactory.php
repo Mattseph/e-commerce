cf72b1b7e8fa7f2744e9b56abe9ebd7dd3689645
Mattseph <105849780+Mattseph@users.noreply.github.com>
2024-10-23 11:27:08 +0800

    Edit: Add Fake Data

--- database/factories/PaymentFactory.php
+++ database/factories/PaymentFactory.php
@@ -2,6 +2,8 @@
 
 namespace Database\Factories;
 
+use App\Models\User;
+use App\Models\Order;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
@@ -16,8 +18,17 @@ class PaymentFactory extends Factory
      */
     public function definition(): array
     {
+        $user_id = User::pluck('id')->toArray();
+        $order_id = Order::pluck('id')->toArray();
+
         return [
-            //
+            'order_id' => fake()->randomNumber($order_id),
+            'amount' => fake()->randomFloat(2),
+            'status' => fake()->randomElement(['Status 1', 'Status 2']),
+            'type' => fake()->randomElement(['Type 1', 'Type 2']),
+            'created_by' => fake()->randomNumber($user_id),
+            'updated_by' => fake()->randomNumber($user_id),
+
         ];
     }
 }
