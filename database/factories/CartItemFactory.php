992b26e1968c74a328db82e198a263f47b493e1c
Mattseph <105849780+Mattseph@users.noreply.github.com>
2024-10-23 11:26:24 +0800

    Edit: Add Fake Data to product_id and quantity Field

--- database/factories/CartItemFactory.php
+++ database/factories/CartItemFactory.php
@@ -2,6 +2,7 @@
 
 namespace Database\Factories;
 
+use App\Models\Product;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
@@ -16,8 +17,12 @@ class CartItemFactory extends Factory
      */
     public function definition(): array
     {
+
+        $product_id = Product::pluck('id')->toArray();
+
         return [
-            //
+            'product_id' => fake()->randomNumber($product_id),
+            'quantity' => fake()->randomNumber(4),
         ];
     }
 }
