a22c75218eb9351f0c12b8045f0dfce260c6eb28
Mattseph <105849780+Mattseph@users.noreply.github.com>
2024-10-23 11:27:21 +0800

    Edit: Add Fake Data

--- database/factories/ProductFactory.php
+++ database/factories/ProductFactory.php
@@ -2,6 +2,9 @@
 
 namespace Database\Factories;
 
+use App\Models\User;
+use App\Models\Brand;
+use App\Models\Category;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
@@ -15,9 +18,23 @@ class ProductFactory extends Factory
      * @return array<string, mixed>
      */
     public function definition(): array
+
     {
+        $category_id = Category::pluck('id')->toArray();
+        $brand_id = Brand::pluck('id')->toArray();
+        $user_id = User::pluck('id')->toArray();
         return [
-            //
+            'category_id' => fake()->randomElement($category_id),
+            'brand_id' => fake()->randomElement($brand_id),
+            'title' => fake()->realText(),
+            'slug' => fake()->realText(),
+            'quantity' => fake()->randomNumber(4),
+            'description' => fake()->realText(),
+            'published' => fake()->boolean(),
+            'inStock' => fake()->boolean(),
+            'price' => fake()->randomFloat(2),
+            'created_by' => fake()->randomNumber($user_id),
+            'updated_by' => fake()->randomNumber($user_id),
         ];
     }
 }
