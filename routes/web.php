b999e3a98188c7466e617f9cb44a1051d8209d1f
Mattseph <105849780+Mattseph@users.noreply.github.com>
2024-10-24 11:48:05 +0800

    Edit: Add Product Route

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use Inertia\Inertia;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Foundation\Application;
+use App\Http\Controllers\ProductController;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\Admin\AdminController;
 use App\Http\Controllers\Admin\AdminAuthController;
@@ -35,5 +36,17 @@ Route::middleware('guest', 'redirect_admin')->prefix('admin')->group(function ()
 
 Route::middleware('auth', 'is_admin')->prefix('admin')->group(function () {
     Route::get('/dashboard', [AdminController::class, 'index'])->name('admin.dashboard');
+
+    Route::prefix('product')->group( function () {
+        Route::get('/', [ProductController::class, 'index'])->name('product.index');
+        Route::get('/{product}', [ProductController::class, 'view'])->name('product.view');
+        Route::get('/create', [ProductController::class, 'create'])->name('product.create');
+        Route::post('/', [ProductController::class, 'store'])->name('product.store');
+        Route::get('/edit/{product}', [ProductController::class, 'edit'])->name('product.edit');
+        Route::put('/{product}', [ProductController::class, 'update'])->name('product.update');
+        Route::delete('/', [ProductController::class, 'destroy'])->name('product.destroy');
+    }
+);
+
 });
 require __DIR__.'/auth.php';
