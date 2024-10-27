534d00a163f4b35b3441ad23fa0d21727af1ee76
Mattseph <105849780+Mattseph@users.noreply.github.com>
2024-10-27 12:26:32 +0800

    Edit: Remove BrandList class

--- resources/js/Pages/Admin/Product/Index.vue
+++ resources/js/Pages/Admin/Product/Index.vue
@@ -159,7 +159,6 @@ onMounted(() => {
                             v-model="brand_id"
                             id="brand"
                             :brands="brands"
-                            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
                         />
                     </div>
 
