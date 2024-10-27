26391a745c505edc182cbfb59c3ede0db87954d0
Mattseph <105849780+Mattseph@users.noreply.github.com>
2024-10-27 12:20:53 +0800

    Edit: Add Select to BrandList

--- resources/js/Components/Admin/Product/BrandList.vue
+++ resources/js/Components/Admin/Product/BrandList.vue
@@ -1,5 +1,26 @@
 <script setup>
+import { ref } from "vue";
+
 defineProps({
-    brand: Object,
+    brands: {
+        type: Array,
+        required: true,
+    },
 });
+
+
+const brandModel = ref("");
 </script>
+
+<template>
+    <select
+        v-model="brandModel"
+        class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+    >
+        <option selected value="" disabled hidden>Select Brand</option>
+
+        <option v-for="brand in brands" :key="brand.id" :value="brand.id">
+            {{ brand.name }}
+        </option>
+    </select>
+</template>
