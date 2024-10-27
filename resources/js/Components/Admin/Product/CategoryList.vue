00e6a78bff8c1b37a52cd638f8ec9da08bf62aeb
Mattseph <105849780+Mattseph@users.noreply.github.com>
2024-10-27 12:25:22 +0800

    Edit: Add Category Select

--- resources/js/Components/Admin/Product/CategoryList.vue
+++ resources/js/Components/Admin/Product/CategoryList.vue
@@ -1,5 +1,23 @@
 <script setup>
+
+import { ref } from 'vue';
+
 defineProps({
-    category: Object,
-})
+    categories: {
+        type: Array,
+        required: true,
+    }
+});
+
+const categoryModel = ref('')
 </script>
+
+<template>
+    <select
+        v-model="categoryModel"
+        class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+    >
+        <option value="" selected disabled hidden>Select category</option>
+        <option v-for="category in categories" :key="category.id" :value="category.id">{{ category.name }}</option>
+    </select>
+</template>
