460f8ab54b3bf745ddf2ef787f3e1171142a2691
Mattseph <105849780+Mattseph@users.noreply.github.com>
2024-10-30 20:13:23 +0800

    Edit: Add Pagination.vue and fields in openEditModal

--- resources/js/Pages/Admin/Product/Index.vue
+++ resources/js/Pages/Admin/Product/Index.vue
@@ -7,8 +7,13 @@ import AdminLayout from "../Layout/AdminLayout.vue";
 import ProductList from "@/Components/Admin/Product/ProductList.vue";
 import BrandList from "@/Components/Admin/Product/BrandList.vue";
 import CategoryList from "@/Components/Admin/Product/CategoryList.vue";
+import Pagination from "@/Components/Admin/Product/Pagination.vue";
 import { Plus } from "@element-plus/icons-vue";
 import { descriptionItemProps } from "element-plus";
+
+onMounted(() => {
+    initFlowbite();
+});
 
 const products = usePage().props.products;
 const brands = usePage().props.brands;
@@ -62,8 +67,18 @@ const openEditModal = (product) => {
     dialogVisible.value = true;
     addModal.value = false;
     editModal.value = true;
-};
 
+    fields.id = product.id;
+    fields.title = product.title;
+    fields.price = product.price;
+    fields.quantity = product.quantity;
+    fields.description = product.description;
+    fields.category_id = product.category_id;
+    fields.brand_id = product.brand_id;
+    fields.inStock = product.inStock;
+    fields.product_images = product.product_images;
+    fields.productImages = product.productImages;
+};
 
 const addNewProduct = async () => {
     const Form = new FormData();
@@ -100,10 +115,6 @@ const addNewProduct = async () => {
         console.log(error.response || error);
     }
 };
-
-onMounted(() => {
-    initFlowbite();
-});
 </script>
 
 <template>
@@ -314,7 +325,7 @@ onMounted(() => {
                                 class="flex items-center justify-center text-white bg-orange-500 hover:bg-yellow-600 focus:ring-4 focus:ring-orange-200 font-medium rounded-lg text-sm px-4 py-2 dark:bg-orange-500 dark:hover:bg-orange-600 focus:outline-none dark:focus:ring-orange-700"
                             >
                                 <svg
-                                    class="h-3.5 w-3.5 mr-2"
+                                    class="h-4 w-4 mr-2 mb-0.5"
                                     fill="currentColor"
                                     viewbox="0 0 20 20"
                                     xmlns="http://www.w3.org/2000/svg"
@@ -521,108 +532,12 @@ onMounted(() => {
                                     :key="product.id"
                                     :product="product"
                                 />
+
                             </tbody>
                         </table>
                     </div>
-                    <nav
-                        class="flex flex-col md:flex-row justify-between items-start md:items-center space-y-3 md:space-y-0 p-4"
-                        aria-label="Table navigation"
-                    >
-                        <span
-                            class="text-sm font-normal text-gray-500 dark:text-gray-400"
-                        >
-                            Showing
-                            <span
-                                class="font-semibold text-gray-900 dark:text-white"
-                                >1-10</span
-                            >
-                            of
-                            <span
-                                class="font-semibold text-gray-900 dark:text-white"
-                                >1000</span
-                            >
-                        </span>
-                        <ul class="inline-flex items-stretch -space-x-px">
-                            <li>
-                                <a
-                                    href="#"
-                                    class="flex items-center justify-center h-full py-1.5 px-3 ml-0 text-gray-500 bg-white rounded-l-lg border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
-                                >
-                                    <span class="sr-only">Previous</span>
-                                    <svg
-                                        class="w-5 h-5"
-                                        aria-hidden="true"
-                                        fill="currentColor"
-                                        viewbox="0 0 20 20"
-                                        xmlns="http://www.w3.org/2000/svg"
-                                    >
-                                        <path
-                                            fill-rule="evenodd"
-                                            d="M12.707 5.293a1 1 0 010 1.414L9.414 10l3.293 3.293a1 1 0 01-1.414 1.414l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 0z"
-                                            clip-rule="evenodd"
-                                        />
-                                    </svg>
-                                </a>
-                            </li>
-                            <li>
-                                <a
-                                    href="#"
-                                    class="flex items-center justify-center text-sm py-2 px-3 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
-                                    >1</a
-                                >
-                            </li>
-                            <li>
-                                <a
-                                    href="#"
-                                    class="flex items-center justify-center text-sm py-2 px-3 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
-                                    >2</a
-                                >
-                            </li>
-                            <li>
-                                <a
-                                    href="#"
-                                    aria-current="page"
-                                    class="flex items-center justify-center text-sm z-10 py-2 px-3 leading-tight text-primary-600 bg-primary-50 border border-primary-300 hover:bg-primary-100 hover:text-primary-700 dark:border-gray-700 dark:bg-gray-700 dark:text-white"
-                                    >3</a
-                                >
-                            </li>
-                            <li>
-                                <a
-                                    href="#"
-                                    class="flex items-center justify-center text-sm py-2 px-3 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
-                                    >...</a
-                                >
-                            </li>
-                            <li>
-                                <a
-                                    href="#"
-                                    class="flex items-center justify-center text-sm py-2 px-3 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
-                                    >100</a
-                                >
-                            </li>
-                            <li>
-                                <a
-                                    href="#"
-                                    class="flex items-center justify-center h-full py-1.5 px-3 leading-tight text-gray-500 bg-white rounded-r-lg border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
-                                >
-                                    <span class="sr-only">Next</span>
-                                    <svg
-                                        class="w-5 h-5"
-                                        aria-hidden="true"
-                                        fill="currentColor"
-                                        viewbox="0 0 20 20"
-                                        xmlns="http://www.w3.org/2000/svg"
-                                    >
-                                        <path
-                                            fill-rule="evenodd"
-                                            d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
-                                            clip-rule="evenodd"
-                                        />
-                                    </svg>
-                                </a>
-                            </li>
-                        </ul>
-                    </nav>
+
+                    <Pagination />
                 </div>
             </div>
         </section>
