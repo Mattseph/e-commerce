8691ea3943427f9b646cf419d5cb54164aea55fa
Mattseph <105849780+Mattseph@users.noreply.github.com>
2024-11-09 13:32:42 +0800

    Edit: Add id, quantity, product_images. Remove: brand_avg

--- app/Http/Resources/ProductResource.php
+++ app/Http/Resources/ProductResource.php
@@ -15,14 +15,17 @@ class ProductResource extends JsonResource
     public function toArray(Request $request): array
     {
         return [
+            'id' => $this->id,
             'title' => $this->title,
             'description' => $this->description,
             'published' => $this->published,
             'inStock' => $this->inStock,
+            'quantity' => $this->quantity,
             'price' => $this->price,
-            'brand_avg' => $this->whenAggregated('brand', 'name', 'avg'),
+            // 'brand_avg' => $this->whenAggregated('brand', 'name', 'avg'),
             'category' => CategoryResource::make($this->whenLoaded('category')),
             'brand' => BrandResource::make($this->whenLoaded('brand')),
+            'product_images' => ProductImageResource::collection($this->whenLoaded('product_images')),
             'created_at' => DateTimeResource::make($this->created_at),
         ];
     }
