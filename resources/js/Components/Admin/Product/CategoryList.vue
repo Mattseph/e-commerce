9c7a66fbfd9060f96fba6b10bd3cb05328c7809d
Mattseph <105849780+Mattseph@users.noreply.github.com>
2024-11-09 13:58:38 +0800

    Edit: Removed category_id props, Added defineModel({})

--- resources/js/Components/Admin/Product/CategoryList.vue
+++ resources/js/Components/Admin/Product/CategoryList.vue
@@ -4,9 +4,11 @@ const props = defineProps({
         type: Array,
         required: true,
     },
-    category_id: {
-        type: Number,
-    }
+});
+
+const model  = defineModel({
+    type: Number,
+    required: true,
 });
 
 
@@ -14,7 +16,7 @@ const props = defineProps({
 </script>
 
 <template>
-    <select v-model="props.category_id"
+    <select v-model="model"
         class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
     >
         <option value="" selected disabled hidden>Select category</option>
