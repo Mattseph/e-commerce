794a62055776cca6963dd90b21caab63c3771f6e
Mattseph <105849780+Mattseph@users.noreply.github.com>
2024-11-10 13:03:46 +0800

    Edit: {id} -> {product}

--- routes/web.php
+++ routes/web.php
@@ -40,14 +40,15 @@ Route::middleware('auth', 'is_admin')->prefix('admin')->group(function () {
     Route::controller(ProductController::class)->prefix('product')->group( function () {
 
         Route::get('/', 'index')->name('admin.product.index');
-        Route::get('/{id}', 'view')->name('admin.product.view');
+        Route::get('/{product}', 'view')->name('admin.product.view');
         Route::get('/create', 'create')->name('admin.product.create');
         Route::post('/', 'store')->name('admin.product.store');
-        Route::get('/edit/{id}', 'edit')->name('admin.product.edit');
-        Route::put('/{id}', 'update')->name('admin.product.update');
-        Route::delete('/{id}', 'destroy')->name('admin.product.destroy');
+        Route::get('/edit/{product}', 'edit')->name('admin.product.edit');
+        Route::put('/{product}', 'update')->name('admin.product.update');
+        Route::delete('/{product}', 'destroy')->name('admin.product.destroy');
     }
 );
+
 
 });
 require __DIR__.'/auth.php';
