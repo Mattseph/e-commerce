e65257e1bac721e7b1efd7751bc46c849be1ceb9
Mattseph <105849780+Mattseph@users.noreply.github.com>
2024-11-12 16:19:12 +0800

    Edit: import Footer

--- resources/js/Pages/User/Index.vue
+++ resources/js/Pages/User/Index.vue
@@ -3,6 +3,7 @@ import { initFlowbite } from "flowbite";
 import { onMounted } from "vue";
 import { Head } from "@inertiajs/vue3";
 import Header from "@/Components/User/Header.vue";
+import Footer from "@/Components/User/Footer.vue";
 
 onMounted(() => {
     initFlowbite();
@@ -12,4 +13,5 @@ onMounted(() => {
 <template>
     <Head title="Product Page"></Head>
     <Header />
+    <Footer />
 </template>
