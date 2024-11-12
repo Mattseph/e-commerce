9e1bff4335298e21d5e564dd4c2c2df3606fa0c4
Mattseph <105849780+Mattseph@users.noreply.github.com>
2024-11-12 15:34:28 +0800

    Edit: Add UserController.php File

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Inertia\Inertia;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Route;
+use Illuminate\Foundation\Application;
+
+class UserController extends Controller
+{
+   
+}
