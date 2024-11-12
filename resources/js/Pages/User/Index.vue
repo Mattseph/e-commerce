7b3bf44630c0714a54d26f9f12d21219b7489df0
Mattseph <105849780+Mattseph@users.noreply.github.com>
2024-11-12 16:18:43 +0800

    Edit: import Header

--- resources/js/Pages/User/Index.vue
+++ resources/js/Pages/User/Index.vue
@@ -1,3 +1,15 @@
-<script script> </script>
+<script setup>
+import { initFlowbite } from "flowbite";
+import { onMounted } from "vue";
+import { Head } from "@inertiajs/vue3";
+import Header from "@/Components/User/Header.vue";
 
-<template></template>
+onMounted(() => {
+    initFlowbite();
+});
+</script>
+
+<template>
+    <Head title="Product Page"></Head>
+    <Header />
+</template>
