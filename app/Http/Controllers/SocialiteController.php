0e15aed6e4890989cdf478fd13765a6d6d94b355
Mattseph <105849780+Mattseph@users.noreply.github.com>
2024-11-13 15:53:47 +0800

    Edit: Add googleAuth function

--- app/Http/Controllers/SocialiteController.php
+++ app/Http/Controllers/SocialiteController.php
@@ -2,13 +2,46 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use PhpParser\Node\Stmt\TryCatch;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use App\Http\Requests\Auth\LoginRequest;
 use Laravel\Socialite\Facades\Socialite;
 
 class SocialiteController extends Controller
 {
-    public function googleLogin() {
+    public function googleLogin()
+    {
         return Socialite::driver('google')->redirect();
+    }
+
+    public function googleAuth()
+    {
+
+
+        try {
+            $googleUser = Socialite::driver('google')->user();
+
+            $user = User::where('google_id', $googleUser->id)->first();
+
+            if ($user) {
+                Auth::login($user);
+            } else {
+                $newUser = User::create([
+                    'google_id' => $googleUser->id,
+                    'name' => $googleUser->name,
+                    'email' => $googleUser->email,
+                    'password' => Hash::make('Password'),
+                ]);
+
+                Auth::login($newUser);
+            }
+            return to_route('dashboard');
+        } catch (\Exception $e) {
+            dd($e);
+        }
     }
 }
