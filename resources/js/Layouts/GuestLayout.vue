684d6da46da288b2648f53c1c336f9de9fd5cbbc
Mattseph <105849780+Mattseph@users.noreply.github.com>
2024-11-13 14:36:37 +0800

    Edit :Add or Sign in with Google Link

--- resources/js/Layouts/GuestLayout.vue
+++ resources/js/Layouts/GuestLayout.vue
@@ -1,6 +1,8 @@
 <script setup>
-import ApplicationLogo from '@/Components/ApplicationLogo.vue';
-import { Link } from '@inertiajs/vue3';
+import ApplicationLogo from "@/Components/ApplicationLogo.vue";
+import { Link } from "@inertiajs/vue3";
+
+const isLoginRoute = window.location.pathname === "/login";
 </script>
 
 <template>
@@ -17,6 +19,32 @@ import { Link } from '@inertiajs/vue3';
             class="mt-6 w-full overflow-hidden bg-white px-6 py-4 shadow-md sm:max-w-md sm:rounded-lg"
         >
             <slot />
+
+
+        </div>
+
+        <div v-if="isLoginRoute" class="flex items-center justify-items-center flex-col gap-4 mt-6 max-w-md w-full">
+            <div class="font-bold text-xl">or</div>
+
+            <Link
+
+                class="w-full flex justify-center items-center text-white ml-2 bg-[#4285F4] hover:bg-[#4285F4]/90 focus:ring-4 focus:outline-none focus:ring-[#4285F4]/50 font-medium rounded-lg text-sm px-5 py-3 dark:focus:ring-[#4285F4]/55 me-2 mb-2"
+            >
+                <svg
+                    class="w-4 h-4 me-2"
+                    aria-hidden="true"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="currentColor"
+                    viewBox="0 0 18 19"
+                >
+                    <path
+                        fill-rule="evenodd"
+                        d="M8.842 18.083a8.8 8.8 0 0 1-8.65-8.948 8.841 8.841 0 0 1 8.8-8.652h.153a8.464 8.464 0 0 1 5.7 2.257l-2.193 2.038A5.27 5.27 0 0 0 9.09 3.4a5.882 5.882 0 0 0-.2 11.76h.124a5.091 5.091 0 0 0 5.248-4.057L14.3 11H9V8h8.34c.066.543.095 1.09.088 1.636-.086 5.053-3.463 8.449-8.4 8.449l-.186-.002Z"
+                        clip-rule="evenodd"
+                    />
+                </svg>
+                Sign in with Google
+            </Link>
         </div>
     </div>
 </template>
