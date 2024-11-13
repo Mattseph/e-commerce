0f1b1980b1ae742ed7264fcd7e26dbcef0a04100
Mattseph <105849780+Mattseph@users.noreply.github.com>
2024-11-13 14:38:09 +0800

    Edit: Add :href=route('auth.google.login')

--- resources/js/Layouts/GuestLayout.vue
+++ resources/js/Layouts/GuestLayout.vue
@@ -27,7 +27,7 @@ const isLoginRoute = window.location.pathname === "/login";
             <div class="font-bold text-xl">or</div>
 
             <Link
-
+                :href="route('auth.google.login')"
                 class="w-full flex justify-center items-center text-white ml-2 bg-[#4285F4] hover:bg-[#4285F4]/90 focus:ring-4 focus:outline-none focus:ring-[#4285F4]/50 font-medium rounded-lg text-sm px-5 py-3 dark:focus:ring-[#4285F4]/55 me-2 mb-2"
             >
                 <svg
