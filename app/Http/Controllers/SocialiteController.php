3e31e5e8d958de583f1c3c63d60e529370c8cdb1
Mattseph <105849780+Mattseph@users.noreply.github.com>
2024-11-13 15:04:29 +0800

    Edit: Add googleLogin() function

--- app/Http/Controllers/SocialiteController.php
+++ app/Http/Controllers/SocialiteController.php
@@ -2,10 +2,13 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Laravel\Socialite\Facades\Socialite;
 
 class SocialiteController extends Controller
 {
-    //
+    public function googleLogin() {
+        return Socialite::driver('google')->redirect();
+    }
 }
