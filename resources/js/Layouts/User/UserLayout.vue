b9214654c5c8ef690a176c58cbe84eade738542d
Mattseph <105849780+Mattseph@users.noreply.github.com>
2024-11-18 20:57:01 +0800

    Edit: MainLayout.vue -> UserLayout.vue

---
+++ resources/js/Layouts/User/UserLayout.vue
@@ -0,0 +1,23 @@
+<script setup>
+
+import { initFlowbite } from "flowbite";
+import { onMounted } from "vue";
+import HeaderLayout from "./HeaderLayout.vue";
+import HeroLayout from "./HeroLayout.vue";
+import FooterLayout from "./FooterLayout.vue";
+
+onMounted(() => {
+    initFlowbite();
+});
+
+</script>
+
+<template>
+    <HeaderLayout />
+
+    <HeroLayout />
+
+    <slot />
+
+    <FooterLayout />
+</template>
