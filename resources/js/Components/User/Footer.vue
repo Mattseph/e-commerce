742bf749f482f5af569874d9c77fee0925b72da4
Mattseph <105849780+Mattseph@users.noreply.github.com>
2024-11-18 15:11:21 +0800

    Edit Removed shadow class

--- resources/js/Components/User/Footer.vue
+++ resources/js/Components/User/Footer.vue
@@ -3,7 +3,7 @@
 <template>
     <footer class="fixed bottom-0 left-0 z-20 w-full">
         <div
-            class="max-w-screen-xl mx-auto p-4 px-10 bg-white shadow md:flex md:items-center md:justify-between md:p-6 dark:bg-gray-800 dark:border-gray-600"
+            class="max-w-screen-xl mx-auto p-4 px-10 bg-white md:flex md:items-center md:justify-between md:p-6 dark:bg-gray-800 dark:border-gray-600"
         >
             <span
                 class="text-sm text-gray-500 sm:text-center dark:text-gray-400"
