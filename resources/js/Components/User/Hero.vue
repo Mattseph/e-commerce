3e1a9c644dec6177de7b9985958de9a5da500ab5
Mattseph <105849780+Mattseph@users.noreply.github.com>
2024-11-18 15:20:59 +0800

    Edit: bg-primary-700 -> bg-orange-500

--- resources/js/Components/User/Hero.vue
+++ resources/js/Components/User/Hero.vue
@@ -45,7 +45,7 @@
             >
                 <a
                     href="#"
-                    class="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-white rounded-lg bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 dark:focus:ring-primary-900"
+                    class="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-white bg-orange-500 rounded-lg hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 dark:focus:ring-primary-900"
                 >
                     Learn more
                     <svg
