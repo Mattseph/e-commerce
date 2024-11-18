197b61121d63068f6bfb74f2e7039c134464227a
Mattseph <105849780+Mattseph@users.noreply.github.com>
2024-11-18 15:10:47 +0800

    Edit: import Hero.vue

--- resources/js/Pages/User/Index.vue
+++ resources/js/Pages/User/Index.vue
@@ -3,6 +3,7 @@ import { initFlowbite } from "flowbite";
 import { onMounted } from "vue";
 import { Head } from "@inertiajs/vue3";
 import Header from "@/Components/User/Header.vue";
+import Hero from "@/Components/User/Hero.vue";
 import Footer from "@/Components/User/Footer.vue";
 
 onMounted(() => {
@@ -12,7 +13,10 @@ onMounted(() => {
 
 <template>
     <Head title="Product Page"></Head>
+
     <Header />
+
+    <Hero />
 
     <div class="bg-white">
         <div
