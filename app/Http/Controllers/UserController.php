0b905ff52deab8cf3bd4d8fea631d0c688714a4f
Mattseph <105849780+Mattseph@users.noreply.github.com>
2024-11-19 14:41:49 +0800

    Edit: _invoke() -> index(), Add: Product

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,15 +3,21 @@
 namespace App\Http\Controllers;
 
 use Inertia\Inertia;
+use App\Models\Product;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Http\Resources\ProductResource;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Foundation\Application;
 
 class UserController extends Controller
 {
-    public function __invoke() {
+    public function index () {
+
+        $products = ProductResource::collection(Product::orderBy('id', 'desc')->get());
+
         return Inertia::render('User/Index', [
+            'products' => $products,
             'canLogin' => Route::has('login'),
             'canRegister' => Route::has('register'),
             'laravelVersion' => Application::VERSION,
