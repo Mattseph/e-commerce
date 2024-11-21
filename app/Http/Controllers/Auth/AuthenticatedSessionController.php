06a75b784a34bee18ce87e55c26ead5eeef22623
Mattseph <105849780+Mattseph@users.noreply.github.com>
2024-11-21 15:21:00 +0800

    Edit: route dashboard -> user.index

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -40,7 +40,7 @@ class AuthenticatedSessionController extends Controller
             return redirect()->intended(route('admin.dashboard', absolute: false));
         }
 
-        return redirect()->intended(route('dashboard', absolute: false));
+        return redirect()->intended(route('user.index', absolute: false));
     }
 
     /**
