70f150ea9912c0f7e27184c52dc42a2a5e195914
Mattseph <105849780+Mattseph@users.noreply.github.com>
2024-11-21 14:21:49 +0800

    Edit: Add Brand Name and Product Image

--- resources/js/Pages/User/Index.vue
+++ resources/js/Pages/User/Index.vue
@@ -31,16 +31,23 @@ console.log(props.products);
                 </h2>
 
                 <div
-
                     class="mt-6 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8"
                 >
                     <div
-                    v-for="product in props.products.data"
-                     class="group relative">
+                        v-for="product in props.products.data"
+                        class="group relative"
+                    >
                         <div
                             class="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-md bg-gray-200 lg:aspect-none group-hover:opacity-75 lg:h-80"
                         >
                             <img
+                                v-if="product.product_images.length > 0"
+                                :src="`/${product.product_images[0].image}`"
+                                alt="Front of men&#039;s Basic Tee in black."
+                                class="h-full w-full object-cover object-center lg:h-full lg:w-full"
+                            />
+                            <img
+                                v-else
                                 src="https://tailwindui.com/plus/img/ecommerce-images/product-page-01-related-product-01.jpg"
                                 alt="Front of men&#039;s Basic Tee in black."
                                 class="h-full w-full object-cover object-center lg:h-full lg:w-full"
@@ -57,7 +64,9 @@ console.log(props.products);
                                         {{ product.title }}
                                     </a>
                                 </h3>
-                                <p class="mt-1 text-sm text-gray-500">Black</p>
+                                <p class="mt-1 text-sm text-gray-500">
+                                    {{ product.brand.name }}
+                                </p>
                             </div>
                             <p class="text-sm font-medium text-gray-900">
                                 ${{ product.price }}
