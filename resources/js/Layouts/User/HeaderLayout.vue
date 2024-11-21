bcfae30b5b33e891a57d63562e4221541b7803b4
Mattseph <105849780+Mattseph@users.noreply.github.com>
2024-11-21 16:25:50 +0800

    Edit: Add route('logout') to Logout

--- resources/js/Layouts/User/HeaderLayout.vue
+++ resources/js/Layouts/User/HeaderLayout.vue
@@ -141,7 +141,9 @@ const auth = usePage().props.auth;
                         </li>
                         <li>
                             <a
-                                href="#"
+                                :href="route('logout')"
+                                method="post"
+                                as="button"
                                 class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white"
                                 >Sign out</a
                             >
