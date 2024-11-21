91dbec5bd057a0fd96cb2c31cade84d591f5a556
Mattseph <105849780+Mattseph@users.noreply.github.com>
2024-11-21 12:47:48 +0800

    Edit: Add {{product.price}}

--- resources/js/Pages/User/Index.vue
+++ resources/js/Pages/User/Index.vue
@@ -31,10 +31,12 @@ console.log(props.products);
                 </h2>
 
                 <div
-                    v-for="product in props.products.data"
+
                     class="mt-6 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8"
                 >
-                    <div class="group relative">
+                    <div
+                    v-for="product in props.products.data"
+                     class="group relative">
                         <div
                             class="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-md bg-gray-200 lg:aspect-none group-hover:opacity-75 lg:h-80"
                         >
@@ -57,7 +59,9 @@ console.log(props.products);
                                 </h3>
                                 <p class="mt-1 text-sm text-gray-500">Black</p>
                             </div>
-                            <p class="text-sm font-medium text-gray-900">$35</p>
+                            <p class="text-sm font-medium text-gray-900">
+                                ${{ product.price }}
+                            </p>
                         </div>
                     </div>
 
