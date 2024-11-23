1d27a89b3a5caf7e60baef3c693793198b9d5eba
Mattseph <105849780+Mattseph@users.noreply.github.com>
2024-11-23 20:38:37 +0800

    Edit: Used InStock() in Product Query

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -14,7 +14,7 @@ class UserController extends Controller
 {
     public function index () {
 
-        $products = ProductResource::collection(Product::orderBy('id', 'desc')->limit(5)->  get());
+        $products = ProductResource::collection(Product::InStock()->orderBy('id', 'desc')->limit(5)->get());
 
         return Inertia::render('User/Index', [
             'products' => $products,
